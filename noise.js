ba9827151e7b25313813931caebd343b4e54cf10
LaurenSky <ljherrer@gmail.com>
2016-11-21 16:52:53 -0800

    added a title

--- noise.js
+++ noise.js
@@ -1,15 +1,18 @@
+var loadPlay = function(sound) {
+  sound.load();
+  sound.play();
+};
+
 $(document).ready(function() {
   $('.note').on('click', function() {
     var noteSelected = $(this).html();
-    alert("You played note: " + noteSelected);
+    // alert("You played note: " + noteSelected);
     var sound = $('#' + noteSelected + 'Audio')[0];
-    sound.load();
-    sound.play();
+    loadPlay(sound);
   });
 
   $('body').keydown(function(event) {
     var sound = $('#' + event.key + 'Audio')[0];
-    sound.load();
-    sound.play();
+    loadPlay(sound);
   });
 });
