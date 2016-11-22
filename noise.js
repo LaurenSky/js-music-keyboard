cb39cde207c391f9c6e8ec5d05baf8b9ae4b84df
LaurenSky <ljherrer@gmail.com>
2016-11-21 16:31:26 -0800

    add jquery and javascript for click and key functions

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+  $('.note').on('click', function() {
+    var noteSelected = $(this).html();
+    alert("You played note: " + noteSelected);
+    var sound = $('#' + noteSelected + 'Audio')[0];
+    sound.load();
+    sound.play();
+  });
+
+  $('body').keydown(function(event) {
+    var sound = $('#' + event.key + 'Audio')[0];
+    sound.load();
+    sound.play();
+  });
 });
